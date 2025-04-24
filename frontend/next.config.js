4998bcc382829e4793e6c5b46be4cc475e3974c5
jalalsadeghi <jalal.a.sadeghi@gmail.com>
2025-04-24 15:10:11 +0200

    Fixed frontend/Deckerfile

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    
-  };
-  
-  module.exports = nextConfig;
-  
+const path = require('path');
+
+module.exports = {
+  webpack(config) {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  }
+};
